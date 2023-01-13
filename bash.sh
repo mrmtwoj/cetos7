dc11a30f568164f2d670ba808cdc793719144272
Mr Mtwoj <mr.mtwoj@gmail.com>
2023-01-13 19:03:40 +0330

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,6 @@
+yum clean all
+yum update
+yum install yum-utils
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+yum repolist
+yum install net-tools
